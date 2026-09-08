9848741192d1b0010f763e9f125087ac896401a0
Adi-UPES <aditya157628@gmail.com>
2026-09-08 19:31:40 +0530

    Create CODE 12.c

---
+++ CODE 12.c
@@ -0,0 +1,36 @@
+Q12: Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
+
+/*
+Sample Test Cases:
+Input 1:
+-5
+Output 1:
+Negative
+
+Input 2:
+0
+Output 2:
+Zero
+
+Input 3:
+10
+Output 3:
+Positive
+
+*/
+#include<stdio.h>
+#include<conio.h>
+main()
+{int a;
+printf("ENTER YOUR NUMBER");
+scanf("%d",&a);
+if(a>0)
+{printf("Number is Positive");
+}
+else if(a==0)
+{printf("Number is ZERO");
+}
+else
+printf("Number is Negative");
+getch();
+}
