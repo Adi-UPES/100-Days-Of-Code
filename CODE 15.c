af2482eab30b454de3d5701f18dc548f0cab4784
Adi-UPES <aditya157628@gmail.com>
2026-09-09 18:35:13 +0530

    Create CODE 15.c

---
+++ CODE 15.c
@@ -0,0 +1,40 @@
+/*Q15: Write a program to input a character and check whether it is an uppercase alphabet, lowercase alphabet, digit, or special character.
+
+/*
+Sample Test Cases:
+Input 1:
+A
+Output 1:
+Uppercase alphabet
+
+Input 2:
+a
+Output 2:
+Lowercase alphabet
+
+Input 3:
+3
+Output 3:
+Digit
+
+Input 4:
+#
+Output 4:
+Special character
+
+*/
+#include <stdio.h>
+main()
+{char ch;
+printf("Enter character");
+scanf("%c",&ch);
+if(ch>='A' && ch<='Z')
+{printf("UPPER CASE CHARACTER");}
+else if(ch>='a' && ch<='z')
+{printf("LOWER CASE CHARACTER");}
+else if(ch>='1' && ch<='9')
+{printf("DIGIT");}
+else{printf("SPECIAL CHARACTER");}
+return 0;
+    
+}
