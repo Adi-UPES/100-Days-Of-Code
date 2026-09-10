7407797e9308c8c5a9a74928a4cfd3a0ae97f9c8
Adi-UPES <aditya157628@gmail.com>
2026-09-10 10:19:23 +0530

    Create CODE 22.c

---
+++ CODE 22.c
@@ -0,0 +1,38 @@
+/*Q22: Write a program to find profit or loss percentage given cost price and selling price.
+
+/*
+Sample Test Cases:
+Input 1:
+1000 1200
+Output 1:
+Profit 20%
+
+Input 2:
+1000 800
+Output 2:
+Loss 20%
+
+Input 3:
+1000 1000
+Output 3:
+No Profit No Loss
+
+*/
+#include<stdio.h>
+#include<conio.h>
+main()
+{float a,b,cp,sp;
+printf("Enter Your Cost price");
+scanf("%f",&cp);
+printf("Enter your Selling price");
+scanf("%f",&sp);
+if(sp>cp)
+{a=(sp-cp)/cp*100;
+printf("PROFIT PERSENTAGE=%0.2f",a);
+}
+else
+{b=(cp-sp)/cp*100;
+printf("LOSS PERSENTAGE=%0.2f",b);
+}
+getch();
+}
